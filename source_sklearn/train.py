7d2357db22fa377d862acc1806b043abca19ecf3
jair <bravosinvest@gmail.com>
2021-01-04 01:16:20 +0000

    Complex Model to Simple Model
    Took so long to train with GridSearch so i changed to LinearSVC to train fast and check what result, project done!

--- source_sklearn/train.py
+++ source_sklearn/train.py
@@ -5,7 +5,7 @@ import os
 import pandas as pd
 
 # sklearn.externals.joblib is deprecated in 0.21 and will be removed in 0.23. 
-# from sklearn.externals import joblib
+#from sklearn.externals import joblib
 # Import joblib package directly
 import joblib
 
@@ -14,6 +14,7 @@ from sklearn.svm import SVC
 from sklearn.metrics import make_scorer
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
+from sklearn.svm import LinearSVC
 
 # Provided model load function
 def model_fn(model_dir):
@@ -42,14 +43,14 @@ if __name__ == '__main__':
     # Do not need to change
     parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
-    parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
+    parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
     
     ## TODO: Add any additional arguments that you will need to pass into your model
     
     # args holds all passed-in arguments
     args = parser.parse_args()
 
-    # Read in csv training file
+    #  Read in csv training file
     training_dir = args.data_dir
     train_data = pd.read_csv(os.path.join(training_dir, "train.csv"), header=None, names=None)
 
@@ -58,27 +59,27 @@ if __name__ == '__main__':
     train_x = train_data.iloc[:,1:]
     
     
-    ## --- Your code here --- ##
-    classificator = SVC()
+    ## I ll use simple classificator because took so long to train a improved model like this ##
+    """classificator = SVC()
     param_grid = {'C': [1e3, 5e3, 1e4, 5e4], 'gamma': [0.0001, 0.005, 0.01, 0.1], }
     scoring = {'AUC': 'roc_auc', 'Accuracy': make_scorer(accuracy_score)}
     # Run the grid search
-    grid_obj = GridSearchCV(classificator, param_grid, scoring=acc_scorer, refit='AUC')
-    grid_obj = grid_obj.fit(X_train, y_train)
+    grid_obj = GridSearchCV(classificator, param_grid, scoring=scoring, refit='AUC')
+    grid_obj = grid_obj.fit(train_x, train_y)
 
     # Set the clf to the best combination of parameters
     model = grid_obj.best_estimator_
 
     # Fit the best algorithm to the data. 
-    model.fit(X_train, y_train)
-    
-    
-    ## TODO: Train the model
     model.fit(train_x, train_y)
-    
+    """
     
     ## --- End of your code  --- ##
     
+    ## Simple Model ##
+    model = LinearSVC()
 
+    ## TODO: Train the model
+    model.fit(train_x, train_y)
     # Save the trained model
     joblib.dump(model, os.path.join(args.model_dir, "model.joblib"))
